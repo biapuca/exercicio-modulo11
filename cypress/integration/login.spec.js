25c038bf6e1bb81f8dc7aa849cb82b351d71069a
Bia Puca <biapuca@gmail.com>
2022-08-24 10:57:35 -0300

    Meu primeiro exercrio de cypress

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade Login', () =>{ 
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+      it('Deve fazer login com sucesso' , () => {
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain' , 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () =>{
+        cy.get('#username').type('alun_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain' , 'Endereço')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error > li').should('contain' , 'Erro')
+    })
+})
